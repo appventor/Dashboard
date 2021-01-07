e0f7a1720839bd967fe2a02dd8b47f53cc1ae64e
ricksondpenha <ricksondpenha@gmail.com>
2021-01-07 13:14:42 +0530

    file and folder structuring

---
+++ lib/pages.dart
@@ -0,0 +1,8 @@
+export 'pages/home/home.dart';
+export 'pages/dashboard/dashboard.dart';
+export 'pages/login/login.dart';
+export 'pages/settings/settings.dart';
+export 'pages/notifications/notifications.dart';
+export 'pages/users/users.dart';
+
+export 'services/services.dart';
