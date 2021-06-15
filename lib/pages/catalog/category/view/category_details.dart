c1628cee4be88f770b677475c1d7dd20efd20e33
Rickson Dpenha <ricksondpenha@gmail.com>
2021-06-15 16:42:09 +0530

    added category page and category details page

---
+++ lib/pages/catalog/category/view/category_details.dart
@@ -0,0 +1,12 @@
+import '../../../../pages.dart';
+
+class CategoryDetails extends StatelessWidget {
+  final String id;
+  const CategoryDetails({Key? key, @PathParam('id') required this.id})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/pages/catalog/category/view/category_page.dart
@@ -0,0 +1,10 @@
+import '../../../../pages.dart';
+
+class CategoryPage extends StatelessWidget {
+  const CategoryPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
