0062bf7d261bafa64e7f294037ebb283e66ef047
Rickson Dpenha <ricksondpenha@gmail.com>
2021-06-15 16:43:32 +0530

    updated home widget

--- lib/pages/home/view/home.dart
+++ lib/pages/home/view/home.dart
@@ -1,17 +1,41 @@
-import 'package:dashboard/pages/home/view/home_mobile.dart';
-import 'package:dashboard/pages/widgets/responsive.dart';
-
 import '../../../pages.dart';
-import 'home_desktop.dart';
-import 'home_tablet.dart';
 
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Responsive(
-      mobile: HomeMobile(),
-      tablet: HomeTablet(),
-      desktop: HomeDesktop(),
+    return AutoTabsScaffold(
+      routes: [
+        DashboardRoute(),
+        CatalogRoute(),
+        OrdersRoute(),
+        UsersRoute(),
+      ],
+      appBarBuilder: (context, tabsRouter) {
+        return AppBar(
+          title: !Responsive.isMobile(context)
+              ? FlutterLogo(
+                  style: FlutterLogoStyle.horizontal,
+                  size: 100,
+                )
+              : null,
+          centerTitle: false,
+          actions: [],
+        );
+      },
+      drawer: Responsive.isMobile(context) ? SideBar() : null,
+      builder: (context, child, animation) {
+        return Responsive.isMobile(context)
+            ? child
+            : Row(children: [
+                Expanded(
+                  child: SideBar(),
+                ),
+                Expanded(
+                  flex: 5,
+                  child: child,
+                ),
+              ]);
+      },
     );
   }
 }
