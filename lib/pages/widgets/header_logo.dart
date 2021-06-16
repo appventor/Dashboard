8b642ca8d1ceda0d2097deb66296bc0c5bfc58ee
Rickson Dpenha <ricksondpenha@gmail.com>
2021-06-16 18:14:45 +0530

    using flutterlogo in header

---
+++ lib/pages/widgets/header_logo.dart
@@ -0,0 +1,13 @@
+import '../../pages.dart';
+
+class HeaderLogo extends StatelessWidget {
+  const HeaderLogo({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return FlutterLogo(
+      style: FlutterLogoStyle.horizontal,
+      size: 100,
+    );
+  }
+}
