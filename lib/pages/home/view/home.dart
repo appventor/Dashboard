9764e888a576fa3cde89d51d36ef71f5f6631451
Rickson Dpenha <ricksondpenha@gmail.com>
2021-06-26 16:31:57 +0530

    make appbar explicit to sidebar

--- lib/pages/home/view/home.dart
+++ lib/pages/home/view/home.dart
@@ -4,33 +4,40 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return AutoTabsScaffold(
+      lazyLoad: true,
       routes: [
         DashboardRoute(),
         CatalogRoute(),
         OrdersRoute(),
         UsersRoute(),
       ],
-      appBarBuilder: (context, tabsRouter) {
-        return AppBar(
-          title: !Responsive.isMobile(context)
-              ? FlutterLogo(
-                  style: FlutterLogoStyle.horizontal,
-                  size: 100,
-                )
-              : null,
-          centerTitle: false,
-          actions: [],
-        );
-      },
       drawer: Responsive.isMobile(context) ? SideBar() : null,
       builder: (context, child, animation) {
         return Responsive.isMobile(context)
-            ? child
+            ? Scaffold(
+                // drawer: SideBar(),
+                appBar: AppBar(
+                  actions: [],
+                ),
+                body: child,
+              )
             : Row(children: [
                 SideBar(),
                 Expanded(
                   flex: 5,
-                  child: child,
+                  child: Scaffold(
+                    appBar: AppBar(
+                      title: !Responsive.isMobile(context)
+                          ? FlutterLogo(
+                              style: FlutterLogoStyle.horizontal,
+                              size: 100,
+                            )
+                          : null,
+                      centerTitle: false,
+                      actions: [],
+                    ),
+                    body: child,
+                  ),
                 ),
               ]);
       },
