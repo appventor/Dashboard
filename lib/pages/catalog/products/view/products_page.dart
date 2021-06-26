3f7ccb52574823bf80deb859449e64601ed1fe68
Rickson Dpenha <ricksondpenha@gmail.com>
2021-06-26 16:19:07 +0530

    added datatable in products page

--- lib/pages/catalog/products/view/products_page.dart
+++ lib/pages/catalog/products/view/products_page.dart
@@ -5,6 +5,71 @@ class ProductsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 8.0),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Text(
+                "Products",
+                style: Theme.of(context).textTheme.headline5,
+              ),
+              Spacer(),
+              // Padding(
+              //   padding: const EdgeInsets.all(16.0),
+              //   child: OutlinedButton(onPressed: () {}, child: Text("COLUMNS")),
+              // ),
+              ElevatedButton(onPressed: () {}, child: Text("CREATE PRODUCT")),
+            ],
+          ),
+          Expanded(
+            child: SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: SingleChildScrollView(
+                scrollDirection: Axis.vertical,
+                child: DataTable(
+                  showCheckboxColumn: true,
+                  onSelectAll: (value) {},
+                  showBottomBorder: true,
+                  columns: [
+                    DataColumn(label: Text('Name')),
+                    DataColumn(label: Text('Type')),
+                    DataColumn(label: Text('Price')),
+                    DataColumn(label: Text('Stock'))
+                  ],
+                  rows: List.generate(
+                      20,
+                      (index) => buildDataRow(
+                          name: 'Mango',
+                          type: 'Fruits',
+                          price: '12$index',
+                          stock: '10')),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  DataRow buildDataRow({
+    required String name,
+    required String type,
+    required String price,
+    required String stock,
+  }) {
+    return DataRow(
+      cells: [
+        DataCell(Text(name)),
+        DataCell(Text(type)),
+        DataCell(Text(price)),
+        DataCell(Text(stock)),
+      ],
+      onSelectChanged: (value) {},
+      selected: false,
+    );
   }
 }
