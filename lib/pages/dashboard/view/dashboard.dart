c87a6e84ce482fffd976e47147a6178172f03c77
Rickson Dpenha <ricksondpenha@gmail.com>
2021-06-26 16:19:37 +0530

    dashboard demo

--- lib/pages/dashboard/view/dashboard.dart
+++ lib/pages/dashboard/view/dashboard.dart
@@ -1,10 +1,45 @@
-import 'package:flutter/material.dart';
+import '../../../pages.dart';
+import 'widgets/my_files.dart';
+import 'widgets/recent_files.dart';
+import 'widgets/storage_details.dart';
 
 class DashboardPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.lime,
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                  flex: 5,
+                  child: Column(
+                    children: [
+                      MyFiles(),
+                      SizedBox(height: 16),
+                      RecentFiles(),
+                      if (Responsive.isMobile(context))
+                        SizedBox(height: defaultPadding),
+                      if (Responsive.isMobile(context)) StarageDetails(),
+                    ],
+                  ),
+                ),
+                if (!Responsive.isMobile(context))
+                  SizedBox(width: defaultPadding),
+                // On Mobile means if the screen is less than 850 we dont want to show it
+                if (!Responsive.isMobile(context))
+                  Expanded(
+                    flex: 2,
+                    child: StarageDetails(),
+                  ),
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
