7bedd370eddabe35ddb0b188841ca2fcc367aec5
Rickson Dpenha <ricksondpenha@gmail.com>
2021-06-28 18:59:00 +0530

    using paginated data table

--- lib/pages/catalog/products/view/widgets/products_table.dart
+++ lib/pages/catalog/products/view/widgets/products_table.dart
@@ -1,52 +1,40 @@
 import '../../../../../pages.dart';
+import 'products_datasource.dart';
 
 class ProductsTable extends StatelessWidget {
   const ProductsTable({Key? key}) : super(key: key);
 
-  DataRow buildDataRow({
-    required String name,
-    required String type,
-    required String price,
-    required String stock,
-  }) {
-    return DataRow(
-      cells: [
-        DataCell(Text(name)),
-        DataCell(Text(type)),
-        DataCell(Text(price)),
-        DataCell(Text(stock)),
-      ],
-      onSelectChanged: (value) {},
-      selected: false,
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: SingleChildScrollView(
-        scrollDirection: Axis.horizontal,
-        child: SingleChildScrollView(
-          scrollDirection: Axis.vertical,
-          child: DataTable(
-            showCheckboxColumn: true,
-            onSelectAll: (value) {},
-            showBottomBorder: true,
-            columns: [
-              DataColumn(label: Text('Name')),
-              DataColumn(label: Text('Type')),
-              DataColumn(label: Text('Price')),
-              DataColumn(label: Text('Stock'))
-            ],
-            rows: List.generate(
-                20,
-                (index) => buildDataRow(
-                    name: 'Mango',
-                    type: 'Fruits',
-                    price: '12$index',
-                    stock: '10')),
+    return SingleChildScrollView(
+      child: PaginatedDataTable(
+        source: ProductsDataSource(context),
+        header: Text('Products'),
+        actions: [
+          IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.delete,
+              )),
+          ElevatedButton(
+            onPressed: () {},
+            child: Text("Add Product"),
           ),
-        ),
+          IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.refresh,
+              )),
+        ],
+        rowsPerPage: 10,
+        columns: [
+          DataColumn(label: Text('Image')),
+          DataColumn(label: Text('Title')),
+          DataColumn(label: Text('Price')),
+          DataColumn(label: Text('Stock')),
+          DataColumn(label: Text('Variants')),
+        ],
+        onPageChanged: (value) {},
       ),
     );
   }
