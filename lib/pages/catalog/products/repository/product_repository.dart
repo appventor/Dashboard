189aa39091ce73d1f8dd10d2899a70435f86f154
Rickson Dpenha <ricksondpenha@gmail.com>
2021-06-28 21:50:09 +0530

    firebase firestore integration

--- lib/pages/catalog/products/repository/product_repository.dart
+++ lib/pages/catalog/products/repository/product_repository.dart
@@ -1,16 +1,27 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:dashboard/services/firestore_provider.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../model/models.dart';
 
-final productRepository =
-    Provider<ProductRepository>((ref) => ProductRepository());
+final productRepository = Provider<ProductRepository>((ref) {
+  FirebaseFirestore _firestoreProvider = ref.watch(firestoreProvider);
+  return ProductRepository(_firestoreProvider);
+});
 
 class ProductRepository {
+  ProductRepository(this._firebaseFirestore);
+
+  final FirebaseFirestore _firebaseFirestore;
+
   Future<List<Product>> getProducts([int page = 1]) async {
     try {
-      final response = await Future.delayed(Duration(seconds: 3), () => {});
-
-      final results = List<Map<String, dynamic>>.from(response['results']);
-
+      var documentList = await _firebaseFirestore
+          .collection("products")
+          .orderBy("title")
+          .limit(10)
+          .get();
+      final results = List<Map<String, dynamic>>.from(documentList.docs);
+      print(results);
       List<Product> products = results
           .map((product) => Product.fromMap(product))
           .toList(growable: false);
