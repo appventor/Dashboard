ef1dabd2d286fabcce4e71a15f699788db90f939
Rickson Dpenha <ricksondpenha@gmail.com>
2021-06-29 14:48:56 +0530

    Product Details: Page layout

--- lib/pages/catalog/products/view/product_details.dart
+++ lib/pages/catalog/products/view/product_details.dart
@@ -1,4 +1,7 @@
 import '../../../../pages.dart';
+import 'widgets/assign_attributes.dart';
+import 'widgets/general_information.dart';
+import 'widgets/product_variants.dart';
 
 class ProductDetails extends StatelessWidget {
   final String id;
@@ -7,6 +10,77 @@ class ProductDetails extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: Text('Product Information'),
+        centerTitle: false,
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: OutlinedButton.icon(
+                style: OutlinedButton.styleFrom(),
+                onPressed: () {},
+                icon: Icon(
+                  Icons.delete,
+                  color: Colors.red,
+                ),
+                label: Text(
+                  'DELETE',
+                  style: Theme.of(context)
+                      .textTheme
+                      .button!
+                      .copyWith(color: Colors.red),
+                )),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: ElevatedButton(
+              onPressed: () {},
+              child: Text(
+                '      SAVE      ',
+              ),
+            ),
+          )
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Expanded(
+                    flex: 5,
+                    child: Column(
+                      children: [
+                        GeneralInformation(),
+                        ProductVariants(),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    flex: 2,
+                    child: Container(
+                      margin: const EdgeInsets.symmetric(horizontal: 8),
+                      padding: const EdgeInsets.all(defaultPadding),
+                      decoration: BoxDecoration(
+                        color: secondaryColor,
+                        borderRadius:
+                            const BorderRadius.all(Radius.circular(10)),
+                      ),
+                      child: AssignAttributes(),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
