e0de786f2f9d635169183df5c0b8d4a84baacaee
Rickson Dpenha <ricksondpenha@gmail.com>
2021-06-29 22:52:33 +0530

    category: categories provider added

---
+++ lib/pages/catalog/category/controller/categories_provider.dart
@@ -0,0 +1,11 @@
+import '../../../../pages.dart';
+import '../model/models.dart';
+import '../repository/category_repository.dart';
+
+final categoriesProvider = FutureProvider<List<Category>>((ref) async {
+  CategoryRepository _categoryRepository = ref.read(categoryRepository);
+  List<Map<String, dynamic>> data = await _categoryRepository.fetchCategories();
+  List<Category> categories =
+      List<Category>.from(data.map((x) => Category.fromMap(x)));
+  return categories;
+});
