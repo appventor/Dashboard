9de0a66aa3ee315c84d1a8257877333e0ef0a3af
Rickson Dpenha <ricksondpenha@gmail.com>
2021-06-29 21:20:37 +0530

    Category: Add and Edit Category Details Page

--- lib/pages/catalog/category/view/category_details.dart
+++ lib/pages/catalog/category/view/category_details.dart
@@ -7,6 +7,96 @@ class CategoryDetails extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(children: [
+            Text('Create Category',
+                style: Theme.of(context).textTheme.headline6),
+          ]),
+          Divider(),
+          Text('General Information'),
+          Row(
+            children: [
+              Container(
+                margin: EdgeInsets.all(8),
+                height: 200,
+                width: 200,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    image: DecorationImage(
+                        fit: BoxFit.cover,
+                        image: NetworkImage('https://picsum.photos/204'))),
+              ),
+              Expanded(
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextField(
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10)),
+                            hintText: 'Title'),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextField(
+                        maxLines: 4,
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10)),
+                            hintText: 'Description'),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          Divider(),
+          Row(
+            children: [
+              Text('Link to Category'),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Tooltip(
+                    message:
+                        'Linking to another category will make this a sub-category',
+                    child: Icon(
+                      Icons.info,
+                      size: 18,
+                    )),
+              )
+            ],
+          ),
+          Spacer(),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: OutlinedButton(
+                  child: Text('Cancel'),
+                  onPressed: () {
+                    context.popRoute();
+                  },
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Text('Save'),
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
