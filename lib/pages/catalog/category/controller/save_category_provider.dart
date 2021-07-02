be6a3cb0cf7a3e06739307c05365293274f29592
Rickson Dpenha <ricksondpenha@gmail.com>
2021-07-02 12:23:19 +0530

    Category: SaveCategory Provider updates

--- lib/pages/catalog/category/controller/save_category_provider.dart
+++ lib/pages/catalog/category/controller/save_category_provider.dart
@@ -4,30 +4,21 @@ import '../../../../pages.dart';
 import '../model/models.dart';
 import '../repository/category_repository.dart';
 
-final categoryTitle = StateProvider<String>((ref) => '');
-final categoryDesc = StateProvider<String>((ref) => '');
-final categoryImagePath = StateProvider<String>((ref) => '');
-final categorySubcategories = StateProvider<List<String>>((ref) => []);
-final categoryProducts = StateProvider<List<String>>((ref) => []);
+final categoryProvider =
+    StateProvider.autoDispose<Category>((ref) => Category.fromMap({}));
 
-final saveCategory = StateNotifierProvider.family
-    .autoDispose<SaveCategory, Category, String>((ref, id) {
+final saveCategory =
+    StateNotifierProvider.autoDispose<SaveCategory, Category>((ref) {
   CategoryRepository _categoryRepository = ref.read(categoryRepository);
   FirebaseStorage storage = ref.read(firebaseStorageProvider);
-  String title = ref.watch(categoryTitle).state;
-  String desc = ref.watch(categoryDesc).state;
-  String imagepath = ref.watch(categoryImagePath).state;
-  List<String> subcategories = ref.watch(categorySubcategories).state;
-  print(title + " , " + desc);
-
+  Category category = ref.watch(categoryProvider).state;
+  if (category.id.isEmpty) {
+    category = category.copyWith(id: ref.read(firestoreIdProvider));
+  }
   return SaveCategory(
-    id: id,
-    title: title,
-    desc: desc,
-    imagePath: imagepath,
-    subcategories: subcategories,
     storage: storage,
     categoryRepository: _categoryRepository,
+    category: category,
   );
 });
 
@@ -35,34 +26,36 @@ class SaveCategory extends StateNotifier<Category> {
   SaveCategory({
     required this.categoryRepository,
     required this.storage,
-    required this.id,
-    required this.title,
-    required this.desc,
-    required this.imagePath,
-    required this.subcategories,
-  }) : super(Category.fromMap({
-          "id": id,
-          "title": title,
-          "desc": desc,
-          "subcategories": subcategories,
-        }));
+    required this.category,
+  }) : super(category);
 
-  final String id, title, desc, imagePath;
-  final List<String> subcategories;
+  final Category category;
   final CategoryRepository categoryRepository;
   final FirebaseStorage storage;
 
-  uploadImage() {
-    UploadTask task = storage
-        .ref('categories/${imagePath.split('/').last}')
-        .putFile(File(imagePath));
-    return task.asStream();
+  validate() {}
+
+  Future<String?> uploadImage() async {
+    try {
+      TaskSnapshot task = await storage
+          .ref('categories/${state.id}.jpg')
+          .putFile(File(state.imagePath!));
+      return task.ref.getDownloadURL();
+    } catch (error) {
+      return null;
+    }
   }
 
   Future<bool> save() async {
-    // uploadImage();
-    print(state.toMap());
-    await categoryRepository.saveCategory(id: state.id, data: state.toMap());
-    return true;
+    return uploadImage().then((imageUrl) async {
+      if (imageUrl != null) {
+        state = state.copyWith(image: imageUrl);
+        print(state.toMap());
+        await categoryRepository.saveCategory(
+            id: state.id, data: state.toMap());
+        return true;
+      } else
+        return false;
+    });
   }
 }
