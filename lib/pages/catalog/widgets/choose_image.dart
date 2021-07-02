28cd9d4ebadcafc0947464fc76c93a06d6177d49
Rickson Dpenha <ricksondpenha@gmail.com>
2021-07-02 12:24:06 +0530

    Integrate macos file selector

--- lib/pages/catalog/widgets/choose_image.dart
+++ lib/pages/catalog/widgets/choose_image.dart
@@ -1,3 +1,7 @@
+import 'dart:io';
+
+import 'package:file_selector_platform_interface/file_selector_platform_interface.dart';
+import 'package:flutter/foundation.dart';
 import 'package:image_picker/image_picker.dart';
 import '../../../../../../pages.dart';
 
@@ -17,13 +21,33 @@ class ChooseImage extends StatefulWidget {
 class _ChooseImageState extends State<ChooseImage> {
   final _picker = ImagePicker();
   PickedFile? _image;
+  String? imagePath;
+
+  Future<String> _openImageFile() async {
+    final typeGroup = XTypeGroup(
+      label: 'images',
+      extensions: ['jpg', 'png'],
+    );
+    final files = await FileSelectorPlatform.instance
+        .openFiles(acceptedTypeGroups: [typeGroup]);
+    final file = files[0];
+    final fileName = file.name;
+    return file.path;
+  }
 
   @override
   Widget build(BuildContext context) {
+    print(widget.imageUrl);
     return GestureDetector(
       onTap: () async {
-        _image = await _picker.getImage(source: ImageSource.gallery);
-        if (_image != null) widget.onSelected(_image!.path);
+        if (kIsWeb) {
+          _image = await _picker.getImage(source: ImageSource.gallery);
+          imagePath = _image?.path;
+          if (imagePath != null) widget.onSelected(imagePath!);
+        } else {
+          imagePath = await _openImageFile();
+          if (imagePath != null) widget.onSelected(imagePath!);
+        }
         setState(() {});
       },
       child: Container(
@@ -35,13 +59,21 @@ class _ChooseImageState extends State<ChooseImage> {
               borderRadius: BorderRadius.circular(10),
               border: Border.all(color: Colors.blue),
               image: widget.imageUrl != null
-                  ? DecorationImage(
-                      fit: BoxFit.cover, image: NetworkImage(widget.imageUrl!))
-                  : _image != null
+                  ? widget.imageUrl!.isNotEmpty
                       ? DecorationImage(
-                          fit: BoxFit.cover, image: NetworkImage(_image!.path))
-                      : null),
-          child: widget.imageUrl == null && _image == null
+                          fit: BoxFit.cover,
+                          image: NetworkImage(widget.imageUrl!))
+                      : imagePath != null
+                          ? kIsWeb
+                              ? DecorationImage(
+                                  fit: BoxFit.cover,
+                                  image: NetworkImage(imagePath!))
+                              : DecorationImage(
+                                  fit: BoxFit.cover,
+                                  image: FileImage(File(imagePath!)))
+                          : null
+                  : null),
+          child: widget.imageUrl == null || imagePath == null
               ? Text('Upload a Image')
               : null),
     );
