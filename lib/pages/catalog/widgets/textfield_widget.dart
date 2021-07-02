f9cd345d3c417052996129021e7e11ab18506767
Rickson Dpenha <ricksondpenha@gmail.com>
2021-07-02 12:22:53 +0530

    get initial text value

--- lib/pages/catalog/widgets/textfield_widget.dart
+++ lib/pages/catalog/widgets/textfield_widget.dart
@@ -3,11 +3,13 @@ import '../../../pages.dart';
 
 class TextFieldWidget extends StatefulWidget {
   final String label;
+  final String text;
   final int maxLines;
   final Function(String) onChanged;
   const TextFieldWidget({
     Key? key,
     required this.label,
+    required this.text,
     this.maxLines = 1,
     required this.onChanged,
   }) : super(key: key);
@@ -18,9 +20,17 @@ class TextFieldWidget extends StatefulWidget {
 
 class _TextFieldWidgetState extends State<TextFieldWidget> {
   Timer? _debounce;
+  late TextEditingController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = TextEditingController(text: widget.text);
+  }
 
   @override
   void dispose() {
+    _controller.dispose();
     _debounce?.cancel();
     super.dispose();
   }
@@ -30,10 +40,12 @@ class _TextFieldWidgetState extends State<TextFieldWidget> {
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: TextField(
+        controller: _controller,
         maxLines: widget.maxLines,
         decoration: InputDecoration(
-            border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
-            hintText: widget.label),
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
+          hintText: widget.label,
+        ),
         onChanged: (value) {
           if (_debounce?.isActive ?? false) _debounce!.cancel();
           _debounce = Timer(
