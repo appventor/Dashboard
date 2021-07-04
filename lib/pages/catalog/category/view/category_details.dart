bf49c9da1cde90cbb8ad12c9c4e939cfc63769c6
Rickson Dpenha <ricksondpenha@gmail.com>
2021-07-04 22:20:57 +0530

    Category: Add loading state

--- lib/pages/catalog/category/view/category_details.dart
+++ lib/pages/catalog/category/view/category_details.dart
@@ -16,68 +16,106 @@ class CategoryDetails extends StatefulWidget {
 }
 
 class _CategoryDetailsState extends State<CategoryDetails> {
+  bool _loading = false;
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text('Create Category',
-                style: Theme.of(context).textTheme.headline6),
-            Divider(),
-            GeneralInformation(),
-            Divider(),
-            LinkCategories(),
-            SizedBox(height: 16),
-            ButtonBar(
+    return Stack(
+      children: [
+        SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: OutlinedButton(
-                    child: Text('Cancel'),
-                    onPressed: () {
-                      context.read(categoryProvider).state =
-                          Category.fromMap({});
-                      context.popRoute();
-                    },
-                  ),
+                Row(
+                  children: [
+                    BackButton(),
+                    Text('Category Details',
+                        style: Theme.of(context).textTheme.headline6),
+                  ],
                 ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: ElevatedButton(
-                    onPressed: () async {
-                      final overlay = LoadingOverlay.of(context);
-                      overlay
-                          .during(context.read(saveCategory.notifier).save())
-                          .then((result) {
-                        if (result) {
+                Divider(),
+                GeneralInformation(),
+                Divider(),
+                LinkCategories(
+                  excludeID: context.read(categoryProvider).state.id,
+                  initialValue:
+                      context.read(categoryProvider).state.subcategories,
+                  onSelected: (var value) {
+                    print(value);
+                    context.read(categoryProvider).state = context
+                        .read(categoryProvider)
+                        .state
+                        .copyWith(
+                            subcategories: value
+                                .map((items) => items.toString())
+                                .toList());
+                  },
+                ),
+                SizedBox(height: 16),
+                ButtonBar(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: OutlinedButton(
+                        child: Text('Cancel'),
+                        onPressed: () {
                           context.read(categoryProvider).state =
                               Category.fromMap({});
-                          context.refresh(categoriesProvider);
-                          scaffoldMessengerKey.currentState!
-                              .showSnackBar(SnackBar(
-                            content: Text("Saved"),
-                            backgroundColor: Colors.green,
-                          ));
                           context.popRoute();
-                        } else
-                          scaffoldMessengerKey.currentState!
-                              .showSnackBar(SnackBar(
-                            content: Text("Error, Please try again"),
-                            backgroundColor: Colors.red,
-                          ));
-                      });
-                    },
-                    child: Text('Save'),
-                  ),
+                        },
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: ElevatedButton(
+                        onPressed: () async {
+                          setState(() {
+                            _loading = true;
+                          });
+                          context
+                              .read(saveCategory.notifier)
+                              .save()
+                              .then((result) {
+                            if (result) {
+                              context.read(categoryProvider).state =
+                                  Category.fromMap({});
+                              context.refresh(categoriesProvider);
+                              scaffoldMessengerKey.currentState!
+                                  .showSnackBar(SnackBar(
+                                content: Text("Saved"),
+                                backgroundColor: Colors.green,
+                              ));
+                              context.popRoute();
+                            } else
+                              scaffoldMessengerKey.currentState!
+                                  .showSnackBar(SnackBar(
+                                content: Text("Error, Please try again"),
+                                backgroundColor: Colors.red,
+                              ));
+                            setState(() {
+                              _loading = false;
+                            });
+                          });
+                        },
+                        child: Text('Save'),
+                      ),
+                    )
+                  ],
                 )
               ],
-            )
-          ],
+            ),
+          ),
         ),
-      ),
+        Visibility(
+            visible: _loading,
+            child: Container(
+              width: double.infinity,
+              height: double.infinity,
+              color: Colors.black54,
+              child: Center(child: CircularProgressIndicator()),
+            )),
+      ],
     );
   }
 }
