2e9f494226a8a20e8464d920098515a4fd13c35e
Rickson Dpenha <ricksondpenha@gmail.com>
2021-07-04 22:33:37 +0530

    Warehouse: Added Warehouses Provider and model

--- lib/pages/warehouse/view/widgets/select_warehouse.dart
+++ lib/pages/warehouse/view/widgets/select_warehouse.dart
@@ -1,3 +1,6 @@
+import 'package:dashboard/pages/warehouse/controller/warehouses_provider.dart';
+import 'package:dashboard/pages/warehouse/models/warehouse_model.dart';
+
 import '../../../../pages.dart';
 
 class SelectWarehouse extends StatefulWidget {
@@ -8,41 +11,46 @@ class SelectWarehouse extends StatefulWidget {
 }
 
 class _SelectWarehouseState extends State<SelectWarehouse> {
-  String _warehouse = 'Kavoor';
+  Warehouse? _warehouse;
+
   @override
   Widget build(BuildContext context) {
-    return DropdownButton<String>(
-      focusColor: Colors.white,
-      value: _warehouse,
-      //elevation: 5,
-      elevation: 20,
-      iconEnabledColor: Colors.white,
-      items: <String>[
-        'Kavoor',
-        'Kulai',
-        'Hampankatta',
-        'Attavar',
-        'Kankanadi',
-        'Kadri',
-        'Kuntikan',
-      ].map<DropdownMenuItem<String>>((String value) {
-        return DropdownMenuItem<String>(
-          value: value,
-          child: Text(
-            value,
-          ),
-        );
-      }).toList(),
-      hint: Text(
-        "Please choose a langauage",
-        style: TextStyle(
-            color: Colors.black, fontSize: 14, fontWeight: FontWeight.w500),
-      ),
-      onChanged: (String? value) {
-        setState(() {
-          _warehouse = value ?? '';
-        });
-      },
-    );
+    return Consumer(builder: (context, watch, child) {
+      AsyncValue<List<Warehouse>> warehousesData = watch(warehousesProvider);
+      return warehousesData.when(
+          data: (warehouses) {
+            print(warehouses.map((e) => e.title).toList());
+            return DropdownButton<Warehouse>(
+              focusColor: Colors.white,
+              value: _warehouse ?? warehouses.first,
+              elevation: 20,
+              iconEnabledColor: Colors.white,
+              items: warehouses
+                  .map<DropdownMenuItem<Warehouse>>((Warehouse value) {
+                return DropdownMenuItem<Warehouse>(
+                  value: value,
+                  child: Text(
+                    value.title,
+                  ),
+                );
+              }).toList(),
+              hint: Text(
+                "Please choose a langauage",
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 14,
+                    fontWeight: FontWeight.w500),
+              ),
+              onChanged: (Warehouse? value) {
+                context.read(warehouseProvider).state = value!;
+                setState(() {
+                  _warehouse = value;
+                });
+              },
+            );
+          },
+          loading: () => SizedBox.shrink(),
+          error: (_, __) => SizedBox.shrink());
+    });
   }
 }
