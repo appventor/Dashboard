8dfd507a7092f8ae53d6230a6f6517784adbcf87
Rickson Dpenha <ricksondpenha@gmail.com>
2021-07-04 22:25:13 +0530

    Product: Product repository updated

--- lib/pages/catalog/products/repository/product_repository.dart
+++ lib/pages/catalog/products/repository/product_repository.dart
@@ -4,31 +4,57 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../model/models.dart';
 
 final productRepository = Provider<ProductRepository>((ref) {
-  FirebaseFirestore _firestoreProvider = ref.watch(firestoreProvider);
-  return ProductRepository(_firestoreProvider);
+  FirebaseFirestore _firestore = ref.watch(firestoreProvider);
+  return ProductRepository(_firestore);
 });
 
 class ProductRepository {
-  ProductRepository(this._firebaseFirestore);
+  ProductRepository(this._firestore);
 
-  final FirebaseFirestore _firebaseFirestore;
+  final FirebaseFirestore _firestore;
 
   Future<List<Product>> getProducts([int page = 1]) async {
     try {
-      var documentList = await _firebaseFirestore
+      QuerySnapshot<Map<String, dynamic>> documentList = await _firestore
           .collection("products")
           .orderBy("title")
           .limit(10)
           .get();
-      final results = List<Map<String, dynamic>>.from(documentList.docs);
-      print(results);
+      final results = List<Map<String, dynamic>>.from(
+          documentList.docs.map((doc) => doc.data()));
       List<Product> products = results
           .map((product) => Product.fromMap(product))
           .toList(growable: false);
       return products;
     } catch (error) {
-      print(error);
+      print("RESULT ERR $error");
       return [];
     }
+  }
+
+  saveProductInCategory() {}
+
+  removeProductInCategory() {}
+
+  saveProductInCollection() {}
+
+  removeProductInCollection() {}
+
+  Future<void> saveProduct(
+      {required String id, required Map<String, dynamic> data}) async {
+    return await _firestore.collection("products").doc(id).set(data);
+  }
+
+  Future<void> updateProduct(
+      {required String id, required Map<String, dynamic> data}) async {
+    return await _firestore.collection("products").doc(id).update(data);
+  }
+
+  Future<void> deleteProduct({required String id}) async {
+    return await _firestore.collection("products").doc(id).delete();
+  }
+
+  Future<void> deleteVariant({required String id}) async {
+    return await _firestore.collection("products").doc(id).update({});
   }
 }
