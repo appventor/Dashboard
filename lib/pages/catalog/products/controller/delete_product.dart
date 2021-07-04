81233dff0c6468c55cc60b7964b27e61a26d4a9e
Rickson Dpenha <ricksondpenha@gmail.com>
2021-07-04 22:23:09 +0530

    Product: add delete functionality

---
+++ lib/pages/catalog/products/controller/delete_product.dart
@@ -0,0 +1,20 @@
+import '../../../../pages.dart';
+import '../repository/product_repository.dart';
+
+final deleteProduct = FutureProvider.family<dynamic, String>((ref, id) async {
+  return ref.read(productRepository).deleteProduct(id: id).then((value) async {
+    return await ref
+        .read(firebaseStorageProvider)
+        .ref("products/$id.jpg")
+        .delete();
+  });
+});
+
+final deleteVariant = FutureProvider.family<dynamic, String>((ref, id) async {
+  return ref.read(productRepository).deleteVariant(id: id).then((value) async {
+    return await ref
+        .read(firebaseStorageProvider)
+        .ref("products/$id.jpg")
+        .delete();
+  });
+});
